821a2f627f21e705ebf2f37dd527b0938fb545d6
sk790 <saurabhk2890@gmail.com>
2024-06-13 10:47:21 +0530

    add static path

--- backend/index.js
+++ backend/index.js
@@ -6,22 +6,32 @@ import authRouter from "./routes/auth.route.js";
 import postRouter from "./routes/post.route.js";
 import commentRouter from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
-const app = express()
+const app = express();
+import path from "path";
 
-app.use(express.json())
+app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin:"*",
-    methods:["GET","POST","PUT","DELETE"]
-}))
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+const __dirname = path.resolve();
 
 //routes
-app.use("/api/auth",authRouter);
-app.use("/api/user",userRouter)
-app.use("/api/post",postRouter)
-app.use("/api/comment",commentRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/post", postRouter);
+app.use("/api/comment", commentRouter);
 
-app.listen(3000,()=>{
-    console.log("server is running")
-    connectToDatabase()
-})
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
+app.listen(3000, () => {
+  console.log("server is running");
+  connectToDatabase();
+});
